f811a46f5a75137c1c9c443be32254b222e25dd8
sjmasikane <sjmasikane@gmail..com>
2021-04-19 09:15:33 +0200

    Initial comment

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  const options = new DocumentBuilder()
+  .setTitle('Test API')
+  .setVersion('000')
+  .setDescription('One by One')
+  .addTag('notes')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api',app,document);
+  await app.listen(3001);
+}
+bootstrap();
