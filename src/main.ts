51da5bb953ea9496e01a20d118f3cab3313fcf28
sjmasikane <sjmasikane@yahoo.com>
2021-04-22 08:16:45 +0200

    01 is a new version

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.enableCors();
   const options = new DocumentBuilder()
   .setTitle('Test API')
-  .setVersion('0')
+  .setVersion('01')
   .setDescription('One by Two')
   .addTag('notes')
   .build();
